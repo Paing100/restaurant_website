8954ae72f27bba46973f4a0aea39120bef24366b
Nair Aditya (2023) ZMAC597 <aditya.nair.2023@live.rhul.ac.uk>
2025-02-08 17:35:30 +0000

    Resolve "Database SQL reconstruction"

--- Project/Backend/src/main/resources/database.sql
+++ Project/Backend/src/main/resources/database.sql
@@ -1,63 +1,75 @@
-DROP TABLE IF EXISTS menu_items CASCADE;
-DROP TABLE IF EXISTS item_category CASCADE;
+DROP TABLE IF EXISTS menu_item_dietary_restrictions CASCADE;
+DROP TABLE IF EXISTS menu_item_allergens CASCADE;
+DROP TABLE IF EXISTS dietary_restrictions CASCADE;
+DROP TABLE IF EXISTS allergens CASCADE;
+DROP TABLE IF EXISTS order_menu_items CASCADE;
+DROP TABLE IF EXISTS order CASCADE;
+DROP TABLE IF EXISTS menu_item CASCADE;
 DROP TABLE IF EXISTS customer CASCADE;
-DROP TABLE IF EXISTS allergies CASCADE;
 
 CREATE TABLE customer (
-    order_no SERIAL PRIMARY KEY,
+    customer_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE customer_menu_items (
-    order_no INT,
-    item_id INT,
-    PRIMARY KEY (order_no, item_id),
-    FOREIGN KEY (order_no) REFERENCES customer(order_no) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES menu_items(item_id) ON DELETE CASCADE
+CREATE TABLE order (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT UNIQUE,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE
 );
 
-CREATE TABLE menu_items (
-    item_id INT PRIMARY KEY,
-    category_id INT,
+CREATE TABLE menu_item (
+    item_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(10,2) NOT NULL,
-    calories INT(5),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id) 
+    in_stock BOOLEAN NOT NULL DEFAULT TRUE,
+    calories INT
 );
 
-CREATE TABLE allergies (
-    allergy_id INT PRIMARY KEY, 
-    name VARCHAR(40) UNIQUE NOT NULL
-);
-
-CREATE TABLE item_allergies (
+CREATE TABLE order_menu_items (
+    order_id INT,
     item_id INT,
-    allergy_id INT,
-    PRIMARY KEY (item_id, allergy_id),
-    FOREIGN KEY (item_id) REFERENCES menu_items(item_id) ON DELETE CASCADE,
-    FOREIGN KEY (allergy_id) REFERENCES allergies(allergy_id) ON DELETE CASCADE
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES order(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES menu_item(item_id) ON DELETE CASCADE
+);
+
+CREATE TABLE allergens (
+    allergen_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE menu_item_allergens (
+    item_id INT,
+    allergen_id INT,
+    PRIMARY KEY (item_id, allergen_id),
+    FOREIGN KEY (item_id) REFERENCES menu_item(item_id) ON DELETE CASCADE,
+    FOREIGN KEY (allergen_id) REFERENCES allergens(allergen_id) ON DELETE CASCADE
 );
 
 CREATE TABLE dietary_restrictions (
-    dietaryRes_id SERIAL PRIMARY KEY,
-    dietaryRes_name VARCHAR(255) UNIQUE NOT NULL
+    restriction_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE menu_item_dietary_restrictions (
     item_id INT,
-    dietaryRes_id INT,
-    PRIMARY KEY (item_id, dietaryRes_id),
-    FOREIGN KEY (item_id) REFERENCES menu_items(item_id) ON DELETE CASCADE,
-    FOREIGN KEY (dietaryRes_id) REFERENCES dietary_restrictions(dietaryRes_id) ON DELETE CASCADE
+    restriction_id INT,
+    PRIMARY KEY (item_id, restriction_id),
+    FOREIGN KEY (item_id) REFERENCES menu_item(item_id) ON DELETE CASCADE,
+    FOREIGN KEY (restriction_id) REFERENCES dietary_restrictions(restriction_id) ON DELETE CASCADE
 );
 
-CREATE TABLE item_category ( 
-    category_id INT PRIMARY KEY,
-    name VARCHAR(100) UNIQUE NOT NULL
-);
+INSERT INTO dietary_restrictions (name) VALUES
+('Vegetarian'),
+('Vegan'),
+('Gluten-Free'),
+('Halal');
 
-INSERT INTO item_category (category_id, name) VALUES
-(1, 'Starters'),
-(2, 'Course');
-
--- rewrite test data
+INSERT INTO allergens (name) VALUES
+('Gluten'),
+('Shellfish'),
+('Dairy'),
+('Nuts'),
+('Soya'),
+('Eggs');
