d27a079cbde9a4d028a5f933e31a6a3a17ea908a
Aditya Nair <zmac597@live.rhul.ac.uk>
2025-02-09 01:53:11 +0000

    Configured Security and enabled H2 console access

--- Project/Backend/src/main/java/rhul/cs2810/config/SecurityConfig.java
+++ Project/Backend/src/main/java/rhul/cs2810/config/SecurityConfig.java
@@ -26,7 +26,13 @@ public class SecurityConfig {
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf((csrf) -> csrf.disable()).cors(withDefaults());
+    http.csrf((csrf) -> csrf.disable())
+        .cors(withDefaults())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/h2-console/**").permitAll() // Allow access to H2 Console
+            .anyRequest().authenticated()
+        )
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())); // Allow H2 Console frames;
 
     return http.build();
   }
