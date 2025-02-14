c3b803c7e4371e298384caf1ddf3a148d71666cb
Thomas04N <tsn04@icloud.com>
2025-02-14 15:11:46 +0000

    Creating an add to cart button for the menu items #2

--- Project/Frontend/src/MenuCard.jsx
+++ Project/Frontend/src/MenuCard.jsx
@@ -1,38 +1,52 @@
-import { Card, CardActionArea, CardContent, Typography, CardMedia} from "@mui/material";
+import { Card, CardActionArea, CardContent, Typography, CardMedia, Button } from "@mui/material";
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { CartContext } from './CartContext';
 
-function MenuCard({item}) {
-  return (<>
-          <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="200"
-                    image={item.img} 
-                    alt={item.name}
-                  />
-                  <CardContent>
-                    <Typography variant="h6">{item.name}</Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {item.description}
-                    </Typography>
-                    <Typography variant="subtitle1" sx={{ marginTop: 1 }}>
-                      Price: {item.price}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      Calories: {item.calories} kcal
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      Allergies: {item.allergens.join(", ")}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      Dietary Rescritions: {item.dietaryRestrictions.join(", ")}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+function MenuCard({ item }) {
+  const { addItemToCart } = useContext(CartContext);
 
-          </>
+  const handleAddToCart = () => {
+    addItemToCart(item);
+  };
+
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={item.img}
+          alt={item.name}
+        />
+        <CardContent>
+          <Typography variant="h6">{item.name}</Typography>
+          <Typography variant="body2" color="textSecondary">
+            {item.description}
+          </Typography>
+          <Typography variant="subtitle1" sx={{ marginTop: 1 }}>
+            Price: {item.price}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            Calories: {item.calories} kcal
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            Allergies: {item.allergens.join(", ")}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            Dietary Restrictions: {item.dietaryRestrictions.join(", ")}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ marginTop: 2 }}
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </Button>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
 
@@ -43,9 +57,9 @@ MenuCard.propTypes = {
     description: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     calories: PropTypes.number.isRequired,
-    allergies: PropTypes.arrayOf(PropTypes.string).isRequired,
+    allergens: PropTypes.arrayOf(PropTypes.string).isRequired,
+    dietaryRestrictions: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 };
-
 
 export default MenuCard;
