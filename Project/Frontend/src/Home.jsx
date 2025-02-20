757fb7bcb6fc33f5eb70e51fa5542d999543812d
KYAW Paing (2023) WLIS441 <wlis441@live.rhul.ac.uk>
2025-02-20 09:09:16 +0000

    #2 remove the test button on the Home page to load the menu item

--- Project/Frontend/src/Home.jsx
+++ Project/Frontend/src/Home.jsx
@@ -2,22 +2,6 @@ import { Typography } from "@mui/material";
 import axios from "axios";
 
 function Home() {
-
-
-  async function testButton() {
-    const response = await axios.post("http://localhost:8080/MenuItems/addMenuItem",
-      {
-        "name": "Burger",
-        "description": "Delicious",
-        "price": 12.03,
-        "calories": 450,
-        "available": true,
-        "allergens": "NUTS",
-        "dietaryRestrictions": "VEGAN"
-      });
-    console.log(response);
-  }
-
   return (
     <>
       <div className="content">
@@ -28,7 +12,6 @@ function Home() {
           align="center"
         >
           Welcome to Oaxaca!!!
-          <button onClick={testButton}>Click here</button>
         </Typography>
       </div>
     </>
