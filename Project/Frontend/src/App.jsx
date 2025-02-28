89afe50f67e868b65f7333962385e6b8e4b84d19
KYAW Paing (2023) WLIS441 <wlis441@live.rhul.ac.uk>
2025-02-28 09:29:26 +0000

    #60 add new components regarding watier editing menu in APp

--- Project/Frontend/src/App.jsx
+++ Project/Frontend/src/App.jsx
@@ -1,42 +1,35 @@
-import { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
 import Menu from './Menu';
 import Login from './Login';
 import Waiter from './Waiter';
+import MenuWaiter from './MenuWaiter'
 import Order from './Order';
-import { CartProvider, CartContext } from './CartContext';
-import CustomerModal from './CustomerModal';
-
-function AppContent() {
-  const { customer } = useContext(CartContext);
-
-  if (!customer) {
-    return <CustomerModal />;
-  }
-
-  return (
-    <div className="app">
-      <NavBar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/waiter" element={<sessionCheck><Waiter /></sessionCheck>} />
-        </Routes>
-      </div>
-    </div>
-  );
-}
+import { CartProvider } from './CartContext';
+import sessionCheck from './sessionCheck'
+import EditMenu from './EditMenu';
 
 function App() {
+
   return (
     <CartProvider>
       <Router>
-        <AppContent />
+        <div className="app">
+          <NavBar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/order" element={<Order />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/waiter" element={<sessionCheck><Waiter /></sessionCheck>} />
+              <Route path="/waiter_menu" element={<sessionCheck><MenuWaiter /></sessionCheck>}/>
+              <Route path="/watier_edit_menu/:id" element={<sessionCheck><EditMenu></EditMenu></sessionCheck>}/>
+            </Routes>
+          </div>
+        </div>
       </Router>
     </CartProvider>
   );
