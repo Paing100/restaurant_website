bcae93004aad9f8a251103da98ab9fad964fb755
KYAW Paing (2023) WLIS441 <wlis441@live.rhul.ac.uk>
2025-02-28 09:33:52 +0000

    #60 hide add to cart button if customer and show edit button if watier

--- Project/Frontend/src/Menu.jsx
+++ Project/Frontend/src/Menu.jsx
@@ -3,7 +3,7 @@ import { Grid, Box, Tabs, Tab, Typography } from "@mui/material";
 import MenuCard from './MenuCard.jsx'
 import Filter from './Filter.jsx'
 
-function Menu() {
+function Menu({isWaiterView}) {
   const [selectedTab, setSelectedTab] = useState(0);
   const [menuItems, setMenuItems] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState([]);
@@ -16,8 +16,6 @@ function Menu() {
       })
       .catch(err => console.error(err));
   }, []);
-
-
 
   const categories = ["Appetizers", "Mains", "Desserts", "Drinks"];
 
@@ -55,47 +53,49 @@ function Menu() {
 
   return (
     <>
-      <Box sx={{ padding: 3 }}>
+    <Box sx={{ padding: 3 }}>
+      { !isWaiterView && (
         <Filter
           selectedFilter={selectedFilter}
           onFilterChange={handleFilterChange}
         />
-        <Tabs
-          value={selectedTab}
-          onChange={handleTabChange}
-          centered
-          textColor="inherit"
-          TabIndicatorProps={{ style: { backgroundColor: '#5762d5' } }}
-          sx={{
-            '& .MuiTab-root': {
-              color: 'white',
-            },
-            '& .Mui-selected': {
-              color: '#5762d5',
-            },
-          }}
-        >
-          {categories.map((category, index) => (
-            <Tab key={index} label={category} />
-          ))}
-        </Tabs>
-        {menuItems === null ? (
-          <Typography>Loading ...</Typography>
-        ) : menuItems.length === 0 ? (
-          <Typography>No matching items found.</Typography>
-        ) : (
-          <Grid container spacing={3} sx={{ marginTop: 2 }}>
-            {menuItems.filter((item) => item.category === selectedTab)
-              .map((item, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                  <MenuCard item={item}></MenuCard>
-                </Grid>
-              ))}
-          </Grid>
-        )}
-      </Box>
-    </>
-  );
+      )}
+      <Tabs
+        value={selectedTab}
+        onChange={handleTabChange}
+        centered
+        textColor="inherit"
+        TabIndicatorProps={{ style: { backgroundColor: '#5762d5' } }}
+        sx={{
+          '& .MuiTab-root': {
+            color: 'white',
+          },
+          '& .Mui-selected': {
+            color: '#5762d5',
+          },
+        }}
+      >
+        {categories.map((category, index) => (
+          <Tab key={index} label={category} />
+        ))}
+      </Tabs>
+      {menuItems === null ? (
+        <Typography>Loading ...</Typography>
+      ) : menuItems.length === 0 ? (
+        <Typography>No matching items found.</Typography>
+      ) : (
+        <Grid container spacing={3} sx={{ marginTop: 2 }}>
+          {menuItems.filter((item) => item.category === selectedTab)
+            .map((item, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <MenuCard item={item} isWaiterView={isWaiterView}></MenuCard>
+              </Grid>
+            ))}
+        </Grid>
+      )}
+    </Box>
+  </>
+);
 }
 
 export default Menu;
