60e67525b09e53ee12e2223d180c34c080d35650
KYAW Paing (2023) WLIS441 <wlis441@live.rhul.ac.uk>
2025-02-28 21:42:55 +0000

    #60 remove the order submitted column from order entity

--- Project/Backend/src/main/resources/schema.sql
+++ Project/Backend/src/main/resources/schema.sql
@@ -18,7 +18,6 @@ CREATE TABLE orders (
     customer_id INT NOT NULL,
     table_num INT NOT NULL,
     order_placed TIMESTAMP,
-    order_submitted BOOLEAN NOT NULL DEFAULT FALSE,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE
 );
 
