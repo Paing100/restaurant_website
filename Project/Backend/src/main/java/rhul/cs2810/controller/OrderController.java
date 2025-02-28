1adad1e6cd8f3d6788715d691c93baf527a80c55
KYAW Paing (2023) WLIS441 <wlis441@live.rhul.ac.uk>
2025-02-28 09:30:51 +0000

    #60 add new methods: submitOrder and getAllOrders for customer and waiter respectively

--- Project/Backend/src/main/java/rhul/cs2810/controller/OrderController.java
+++ Project/Backend/src/main/java/rhul/cs2810/controller/OrderController.java
@@ -1,8 +1,17 @@
 package rhul.cs2810.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import rhul.cs2810.model.Order;
 import rhul.cs2810.service.OrderService;
 
@@ -33,9 +42,16 @@ public class OrderController {
     return ResponseEntity.ok("Item removed from order");
   }
 
-  @PostMapping("/orders/submit")
-  public ResponseEntity<String> submitOrder(@RequestBody Order order) {
-    orderService.submitOrder(order);
+  @PostMapping("/Order/{orderId}/submitOrder")
+  public ResponseEntity<String> submitOrder(@PathVariable int orderId) {
+    orderService.submitOrder(orderId);
     return ResponseEntity.ok("Order submitted successfully");
   }
+
+  @GetMapping("/Order/getAllOrders")
+  public ResponseEntity<List<Order>> getAllOrders() {
+    List<Order> orders = orderService.getAllOrders();
+    return ResponseEntity.ok(orders);
+  }
+
 }
