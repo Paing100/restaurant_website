75f8e966892e9cfe6385dc9b83b86b76f4f16cbe
Aditya Nair <zmac597@live.rhul.ac.uk>
2025-03-03 12:15:12 +0000

    added redirection fofor kitchen staff to correct page on login

--- Project/Frontend/src/Login.jsx
+++ Project/Frontend/src/Login.jsx
@@ -1,6 +1,6 @@
-import { Button, TextField, Snackbar, Alert } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import {Button, TextField, Snackbar, Alert} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 import axios from "axios";
 
 function Login() {
@@ -25,13 +25,18 @@ function Login() {
         const expirationTime = new Date().getTime() + 30 * 60 * 1000;
         localStorage.setItem("sessionExpiration", expirationTime);
 
-        const { firstName, role } = response.data;
+        const {firstName, role} = response.data;
         localStorage.setItem("userName", firstName);
         localStorage.setItem("userRole", role);
-        navigate("/waiter");
+        if (role === "WAITER") {
+          navigate("/waiter");
+        } else if (role === "KITCHEN") {
+          navigate("/kitchen_staff");
+        }
       }
     } catch (error) {
-      setMessage("Error: Could not connect to the server OR Wrong credentials" + error);
+      setMessage("Error: Could not connect to the server OR Wrong credentials"
+          + error);
       setSeverity("error");
       setOpen(true);
     }
@@ -42,99 +47,99 @@ function Login() {
   };
 
   return (
-    <>
-      <div
-        className="login"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "300px",
-          gap: "16px",
-        }}
-      >
-        <div className="username">
-          <TextField
-            id="outlined-basic"
-            label="User ID"
-            variant="outlined"
-            onChange={(e) => setUserId(e.target.value)}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'white', // Dark grey border
-                },
-                '&:hover fieldset': {
-                  borderColor: 'white', // Lighter grey on hover
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'white', // Even lighter grey when focused
-                },
-              },
-              '& .MuiInputLabel-root': {
-                color: 'white', // Dark grey text color
-              },
-              '& .MuiInputLabel-root.Mui-focused': {
-                color: 'white', // Lighter grey text color when focused
-              },
-              '& .MuiInputBase-input': {
-                color: 'white', // Dark grey text color
-              },
+      <>
+        <div
+            className="login"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "300px",
+              gap: "16px",
             }}
-          />
-        </div>
-        <div className="password">
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            onChange={(e) => setPassword(e.target.value)}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'white', // Dark grey border
-                },
-                '&:hover fieldset': {
-                  borderColor: 'white', // Lighter grey on hover
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'white', // Even lighter grey when focused
-                },
-              },
-              '& .MuiInputLabel-root': {
-                color: 'white', // Dark grey text color
-              },
-              '& .MuiInputLabel-root.Mui-focused': {
-                color: 'white', // Lighter grey text color when focused
-              },
-              '& .MuiInputBase-input': {
-                color: 'white',
-              },
-            }}
-          />
-        </div>
-        <Button
-          variant="contained"
-          onClick={handleLogin}
-          sx={{
-            backgroundColor: '#333', // Dark grey background
-            color: 'white', // White text color
-            '&:hover': {
-              backgroundColor: '#666', // Lighter grey on hover
-            },
-          }}
         >
-          Login
-        </Button>
-      </div>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
-          {message}
-        </Alert>
-      </Snackbar>
-    </>
+          <div className="username">
+            <TextField
+                id="outlined-basic"
+                label="User ID"
+                variant="outlined"
+                onChange={(e) => setUserId(e.target.value)}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: 'white', // Dark grey border
+                    },
+                    '&:hover fieldset': {
+                      borderColor: 'white', // Lighter grey on hover
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: 'white', // Even lighter grey when focused
+                    },
+                  },
+                  '& .MuiInputLabel-root': {
+                    color: 'white', // Dark grey text color
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white', // Lighter grey text color when focused
+                  },
+                  '& .MuiInputBase-input': {
+                    color: 'white', // Dark grey text color
+                  },
+                }}
+            />
+          </div>
+          <div className="password">
+            <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: 'white', // Dark grey border
+                    },
+                    '&:hover fieldset': {
+                      borderColor: 'white', // Lighter grey on hover
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: 'white', // Even lighter grey when focused
+                    },
+                  },
+                  '& .MuiInputLabel-root': {
+                    color: 'white', // Dark grey text color
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white', // Lighter grey text color when focused
+                  },
+                  '& .MuiInputBase-input': {
+                    color: 'white',
+                  },
+                }}
+            />
+          </div>
+          <Button
+              variant="contained"
+              onClick={handleLogin}
+              sx={{
+                backgroundColor: '#333', // Dark grey background
+                color: 'white', // White text color
+                '&:hover': {
+                  backgroundColor: '#666', // Lighter grey on hover
+                },
+              }}
+          >
+            Login
+          </Button>
+        </div>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity={severity} sx={{width: '100%'}}>
+            {message}
+          </Alert>
+        </Snackbar>
+      </>
   );
 }
 
