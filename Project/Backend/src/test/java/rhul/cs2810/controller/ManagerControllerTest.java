a92e1ef46cf566303147dbb1a50ea4301a513f42
Paing Thet Kyaw <paingthetkyaw.me@gmail.com>
2025-06-03 15:56:12 +0100

    #11 edit Mock to MockBean so that Spring knows to inject this bean

--- Project/Backend/src/test/java/rhul/cs2810/controller/ManagerControllerTest.java
+++ Project/Backend/src/test/java/rhul/cs2810/controller/ManagerControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -41,7 +42,7 @@ public class ManagerControllerTest {
   @Mock
   private OrderService orderService;
 
-  @Mock
+  @MockBean
   private ManagerService managerService;
 
   @Autowired
