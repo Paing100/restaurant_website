391654518b644da227fb544869993f18a2a40275
Paing Thet Kyaw <paingthetkyaw.me@gmail.com>
2025-06-12 09:58:03 +0100

    #43 reuse back button in the UI

--- Project/Frontend/src/AllOrders.jsx
+++ Project/Frontend/src/AllOrders.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, useContext, useCallback } from 'react';
-import { Box, Typography, List, ListItem, ListItemText, Divider, Grid, Paper, Button, IconButton } from '@mui/material';
+import { Box, Typography, List, ListItem, ListItemText, Divider, Grid, Paper, IconButton } from '@mui/material';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { CartContext } from './CartContext/CartContextContext.jsx';
 import useWebSocket from './useWebSocket.jsx';
+import BackButton from './BackButton.jsx';
 
 const AllOrders = () => {
     // State to store orders and the currently expanded order ID
@@ -60,12 +61,9 @@ const AllOrders = () => {
         <Box sx={{ padding: 3 }}>
             {/*Header with Back and Refresh button*/}
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 3 }}>
-                <Button
-                    onClick={() => window.history.back()}
-                    sx={{ backgroundColor: '#333', color: 'white', '&:hover': { backgroundColor: 'darkgray' } }}
-                >
-                    ← Back
-                </Button>
+                <Box>
+                    <BackButton />
+                </Box>
                 <IconButton
                     onClick={() => fetchOrders()} // Call fetchOrders to refresh the orders
                     sx={{
